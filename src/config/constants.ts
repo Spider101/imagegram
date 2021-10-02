45c372d0ff6df2e9fe2917b1312a359b3d5fc49a
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-02 12:35:44 +0530

    add database config constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
 
@@ -5,3 +9,10 @@ export const SERVER = {
     hostname: SERVER_HOSTNAME,
     port: SERVER_PORT
 };
+
+const DB_PASSWORD = process.env.MONGODB_PASSWORD;
+const DB_NAME = 'Imagegram';
+
+export const DB = {
+    uri: `mongodb+srv://adminUser:${DB_PASSWORD}@imagegram.8tj6w.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+}
