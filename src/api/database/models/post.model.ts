0d4522d511382d61a6f892aae696adb8868ceb6e
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-03 20:51:01 +0530

    update Post schema to include list of comment refs to populate

--- src/api/database/models/post.model.ts
+++ src/api/database/models/post.model.ts
@@ -1,11 +1,15 @@
-import { CommentSchema } from './comment.model';
-import { PostDocument } from '../../interfaces';
 import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+import { PostDocument } from '../../interfaces';
 
 const PostSchema = new mongoose.Schema({
     caption: { type: String, required: true },
     creator: { type: String, required: true },
-    image: { type: String, required: true }
+    image: { type: String, required: true },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }]
 }, { timestamps: true });
+
+PostSchema.plugin(mongoosePaginate);
 
 export const Post = mongoose.model<PostDocument>("Post", PostSchema);
--- src/api/interfaces/post.interface.ts
+++ src/api/interfaces/post.interface.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import { CommentDocument } from './comment.interface';
 
 export interface PostDocument extends mongoose.Document {
     caption: string,
     creator: string,
-    image: string
+    image: string,
+    comments: Array<string>
 }
