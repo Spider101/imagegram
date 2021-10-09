e3a8bb2819e4eb0691bf295218182e54755a17eb
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-09 18:37:24 +0530

    refactor schemas to include registration of middlewares on them

--- src/api/database/schemas/comment.schema.ts
+++ src/api/database/schemas/comment.schema.ts
@@ -1,7 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose, { Connection, Schema } from 'mongoose';
 
-export const CommentSchema = new mongoose.Schema({
-    content: { type: String, required: true },
-    creator: { type: String, required: true },
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
-}, { timestamps: true });
+import { CommentDocument } from '../../interfaces';
+
+export default function buildSchema(connection: Connection): Schema {
+    const CommentSchema = new mongoose.Schema(
+        {
+            content: { type: String, required: true },
+            creator: { type: String, required: true },
+            postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+        },
+        { timestamps: true }
+    );
+
+    // register middleware
+    CommentSchema.post('save', async function (comment: CommentDocument) {
+        // find the associated post document and update it such that the new comment's id
+        // is added to the comments property and the numComments property is incremented by one as well
+        await connection.model('Post').findOneAndUpdate(
+            { _id: comment.postId },
+            {
+                $push: { comments: comment._id },
+                $inc: { numComments: 1 }
+            }
+        );
+    });
+
+    return CommentSchema;
+}
--- src/api/database/schemas/post.schema.ts
+++ src/api/database/schemas/post.schema.ts
@@ -1,9 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose, { Connection, Query, Schema } from 'mongoose';
+import { removeImage } from '../../helpers';
+import { PostDocument } from '../../interfaces';
 
-export const PostSchema = new mongoose.Schema({
-    caption: { type: String, required: true },
-    creator: { type: String, required: true },
-    image: { type: String, required: true },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-    numComments: { type: Number, index: true }
-}, { timestamps: true });
+export default function buildSchema(connection: Connection): Schema {
+    const PostSchema = new mongoose.Schema(
+        {
+            caption: { type: String, required: true },
+            creator: { type: String, required: true },
+            image: { type: String, required: true },
+            comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+            numComments: { type: Number, index: true }
+        },
+        { timestamps: true }
+    );
+
+    // register middleware
+    PostSchema.pre<Query<PostDocument, PostDocument>>('deleteMany', async function () {
+        const post = await this.model.findOne(this.getFilter());
+
+        // delete all comments on post and remove associated image from storage
+        await connection.model('Comment').deleteMany({ _id: { $in: post.comments } });
+        await removeImage(post.image);
+    });
+
+    return PostSchema;
+}
