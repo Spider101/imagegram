c8f818db8604b8fa58065cb401cde948539613c7
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-21 20:47:21 +0530

    add testDao function for getting id of a new/existing post from the db

--- src/integration-tests/db.ts
+++ src/integration-tests/db.ts
@@ -1,17 +1,28 @@
 import { Connection, Model } from 'mongoose';
 
 import { AccountDocument } from '../api/interfaces/account';
+import { PostDocument } from '../api/interfaces/post';
 
 export interface MongoTestDAO {
     getAccountIdFromDb(accountName: string): Promise<string>;
+    getPostIdFromDb(accountId: string): Promise<string>;
 }
 
 async function getNewAccount(accountModel: Model<AccountDocument>, accountName: string) {
     return await accountModel.create({ name: accountName });
 }
 
+async function getNewPost(postModel: Model<PostDocument>, accountId: string) {
+    return await postModel.create({
+        caption: 'fake post caption',
+        creator: accountId,
+        image: 'fake image key'
+    });
+}
+
 export function getMongoTestDAO(connection: Connection): MongoTestDAO {
     const accountModel: Model<AccountDocument> = connection.model<AccountDocument>('Account');
+    const postModel: Model<PostDocument> = connection.model<PostDocument>('Post');
     return {
         getAccountIdFromDb: async accountName => {
             const account = await accountModel.findOne({ name: accountName });
@@ -20,6 +31,14 @@ export function getMongoTestDAO(connection: Connection): MongoTestDAO {
                 return newAccount._id;
             }
             return account._id;
+        },
+        getPostIdFromDb: async accountId => {
+            const post = await postModel.findOne({ creator: accountId });
+            if (post == null) {
+                const newPost = await getNewPost(postModel, accountId);
+                return newPost._id;
+            }
+            return post._id;
         }
     };
 }
