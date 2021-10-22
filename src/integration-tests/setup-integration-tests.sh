c269d94e1b8b1ac4e6485121cfa4525a4e4926b6
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-22 20:19:46 +0530

    update setup script to create text file and introduce variable for script current dir

--- src/integration-tests/setup-integration-tests.sh
+++ src/integration-tests/setup-integration-tests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mkdir -p image-storage resources
+SCRIPT_DIR=`dirname "$0"`
+mkdir -p ${SCRIPT_DIR}/image-storage ${SCRIPT_DIR}/resources
+
+export TEST_TEXT_FNAME='test-text-file.txt'
+touch ${SCRIPT_DIR}/resources/${TEST_TEXT_FNAME}
+
 export TEST_JPEG_FNAME='test-jpeg-image.jpg'
-curl -o resources/${TEST_JPEG_FNAME} http://lorempixel.com/400/200
+curl -o ${SCRIPT_DIR}/resources/${TEST_JPEG_FNAME} http://lorempixel.com/400/200
