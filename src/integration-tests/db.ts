4398638cb1c39bb39a32392bfc07918f92f3b3a3
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-22 19:50:53 +0530

    add init and shutdown methods to mongo test DAO

--- src/integration-tests/db.ts
+++ src/integration-tests/db.ts
@@ -1,11 +1,18 @@
+import { Express } from 'express';
+import MongoMemoryServer from 'mongodb-memory-server-core';
 import { Connection, Model } from 'mongoose';
 
+import buildApplication from '../api/app';
+import getDbConnection from '../api/database/connect';
 import { AccountDocument } from '../api/interfaces/account';
 import { PostDocument } from '../api/interfaces/post';
+import { LOG } from '../config';
 
 export interface MongoTestDAO {
+    init(): Promise<Express>;
     getAccountIdFromDb(accountName: string): Promise<string>;
     getPostIdFromDb(accountId: string): Promise<string>;
+    shutDown(): Promise<void>;
 }
 
 async function getNewAccount(accountModel: Model<AccountDocument>, accountName: string) {
@@ -20,11 +27,23 @@ async function getNewPost(postModel: Model<PostDocument>, accountId: string) {
     });
 }
 
-export function getMongoTestDAO(connection: Connection): MongoTestDAO {
-    const accountModel: Model<AccountDocument> = connection.model<AccountDocument>('Account');
-    const postModel: Model<PostDocument> = connection.model<PostDocument>('Post');
+export function getMongoTestDAO(): MongoTestDAO {
+    let connection: Connection;
+    let mongoServer: MongoMemoryServer;
     return {
+        init: async () => {
+            mongoServer = await MongoMemoryServer.create();
+            const connectionString: string = mongoServer.getUri();
+            LOG.info(
+                `Started the mongo server for integration tests using ${connectionString} as the connection string`
+            );
+
+            connection = getDbConnection(connectionString);
+            const app: Express = buildApplication(connection);
+            return app;
+        },
         getAccountIdFromDb: async accountName => {
+            const accountModel: Model<AccountDocument> = connection.model<AccountDocument>('Account');
             const account = await accountModel.findOne({ name: accountName });
             if (account == null) {
                 const newAccount = await getNewAccount(accountModel, accountName);
@@ -33,12 +52,25 @@ export function getMongoTestDAO(connection: Connection): MongoTestDAO {
             return account._id;
         },
         getPostIdFromDb: async accountId => {
+            const postModel: Model<PostDocument> = connection.model<PostDocument>('Post');
             const post = await postModel.findOne({ creator: accountId });
             if (post == null) {
                 const newPost = await getNewPost(postModel, accountId);
                 return newPost._id;
             }
             return post._id;
+        },
+        shutDown: async () => {
+            if (connection) {
+                LOG.info('Shutting down mongoose connection...');
+                await connection.close();
+            }
+
+            // stop the mongo server after all the tests finish running
+            if (mongoServer) {
+                LOG.info('Stopping mongo server for integration tests...');
+                await mongoServer.stop();
+            }
         }
     };
 }
