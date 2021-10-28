28c4ac4967c207906adcf28c370cbbe076320d65
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-28 21:04:19 +0530

    fix return type in account service interface

--- src/api/interfaces/account/service.interface.ts
+++ src/api/interfaces/account/service.interface.ts
@@ -1,8 +1,8 @@
-import { Query, DocumentDefinition } from 'mongoose';
+import { DocumentDefinition } from 'mongoose';
 
 import { AccountDocument } from './document.interface';
 
 export interface AccountService {
     createAccount(account: DocumentDefinition<AccountDocument>): Promise<AccountDocument>;
-    deleteAccount(actountId: string): Promise<Query<AccountDocument, AccountDocument>>;
+    deleteAccount(actountId: string): Promise<AccountDocument>
 }
