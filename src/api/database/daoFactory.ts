7221eaf95c646fe6a0edc3ea0080ead7d4055840
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-10-29 10:40:53 +0530

    add comment DAO to dao factory

--- src/api/database/daoFactory.ts
+++ src/api/database/daoFactory.ts
@@ -1,17 +1,21 @@
 import { Connection } from 'mongoose';
 import { AccountDAO, getAccountDAO, getPostDAO, IPostDAO } from './dao';
+import { getCommentDAO, ICommentDAO } from './dao/comment.dao';
 
 export interface IDAOFactory {
     getAccountDAO(): AccountDAO;
     getPostDAO(): IPostDAO;
+    getCommentDAO(): ICommentDAO;
 }
 
 export default function getDAOFactory(connection: Connection): IDAOFactory {
     const accountDAO: AccountDAO = getAccountDAO(connection);
     const postDAO: IPostDAO = getPostDAO(connection);
+    const commentDAO: ICommentDAO = getCommentDAO(connection);
 
     return {
         getAccountDAO: () => accountDAO,
-        getPostDAO: () => postDAO
+        getPostDAO: () => postDAO,
+        getCommentDAO: () => commentDAO
     };
 }
